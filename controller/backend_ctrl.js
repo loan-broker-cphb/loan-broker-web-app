faa15d712ac6fd5085c11593ca2d2bdc49b66de3
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-11-21 00:49:23 +0100

    connected to the loan-broker backend

--- controller/backend_ctrl.js
+++ controller/backend_ctrl.js
@@ -1,14 +1,13 @@
 let axios = require('axios')
-let backendUrl = 'http://localhost:8080/loanbroker/' // NOTE: THIS HAS TO BE CHANGED...
+let backendUrl = 'http://localhost:8081/quote'
 let ctrl = {}
 
 ctrl.sendLoanRequest = async function(loan){
   try{
+    loan['loanAmount'] = parseFloat(loan.loanAmount)
+    loan['loanDuration'] = parseInt(loan.loanDuration)
     console.log(`LOAN REQUEST TO SEND: ${JSON.stringify(loan)}`);
-    // NOTE: This has to be changed when backend is ready
-    let res = ctrl.saveLoan(loan)
-    //-----------------------------------------------
-    // let res = await axios.post(`${backendUrl}loanRequest`, loan)
+    let res = await axios.post(`${backendUrl}loanRequest`, loan)
     console.log(`RESULT: ${JSON.stringify(res, null, 2)}`);
     return res
   }catch(e){
@@ -22,10 +21,7 @@ ctrl.sendLoanRequest = async function(loan){
 
 ctrl.getLoanReply = async function(ssn){
   try{
-    // NOTE: This has to be changed when backend is ready
-    let res = ctrl.getLoan(ssn)
-    //-----------------------------------------------
-    // let res = await axios.get(`${backendUrl}loanreply/${ssn}`)
+    let res = await axios.get(`${backendUrl}/${ssn}`)
     console.log(`RESULT: ${JSON.stringify(res, null, 2)}`);
     return res
   }catch(e){
@@ -36,54 +32,5 @@ ctrl.getLoanReply = async function(ssn){
     }
   }
 }
-
-//---------------PLAYGROUND - fake response--------------------------
-ctrl.saveLoan = function(loan) {
-  try {
-    return {
-      'statusCode': 200,
-      'message': 'loan request has been sent'
-    }
-  } catch (e) {
-    console.log('saveToLog ERROR:' + e.toString())
-    return {
-      'statusCode': 400,
-      'errorMessage': e.toString()
-    }
-  }
-}
-
-ctrl.getLoan = function(ssn){
-  try {
-    return {
-      'statusCode': 200,
-      'loanReply': {
-        'ssn': '110292-1234',
-        'banks': [
-          {
-            'name': 'cphbusiness.bankJSON',
-            'interestRate': 3.5
-          },
-          {
-            'name': 'group4.bankRABBIT',
-            'interestRate': 0.5
-          },
-          {
-            'name': 'group4.bankREST',
-            'interestRate': 0.5
-          }
-        ]
-      }
-    }
-  } catch (e) {
-    console.log('saveToLog ERROR:' + e.toString())
-    return {
-      'statusCode': 400,
-      'errorMessage': e.toString()
-    }
-  }
-}
-
-
 
 module.exports = ctrl
