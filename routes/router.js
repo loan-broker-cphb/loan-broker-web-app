73e72319dd116aed22c51be62a1944b992d26376
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-11-22 22:38:51 +0100

    Added Find Loan Reply

--- routes/router.js
+++ routes/router.js
@@ -4,15 +4,15 @@ let ctrl = require('../controller/backend_ctrl')
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index')
 });
 
-router.get('/loanrequest', function(req, res, next) {
+router.get('/loanrequest', function(req, res) {
   res.render('loanrequest')
 });
 
-router.post('/loanrequest', async function(req, res, next) {
+router.post('/loanrequest', async function(req, res) {
   let loan = req.body
   let result = await ctrl.sendLoanRequest(loan)
   if (result.statusCode == 200) {
@@ -22,7 +22,7 @@ router.post('/loanrequest', async function(req, res, next) {
     })
   } else if (result.statusCode == 400) {
     res.render('error', {
-      'message': result.errorMessage
+      'message': 'Invalid SSN'
     })
   }
 
@@ -38,9 +38,27 @@ router.get('/loanreply/:ssn', async function(req, res, next) {
     })
   } else if (result.statusCode == 400) {
     res.render('error', {
-      'message': result.errorMessage
+      'message': 'Invalid SSN'
     })
   }
+});
+
+router.get('/search/loanreply', async function(req, res){
+  res.render('findloan')
+})
+
+router.post('/search/loanreply', async function(req, res) {
+  let result = await ctrl.getLoanReply(req.body.ssn)
+  if (result.statusCode == 200) {
+    res.render('loanreply', {
+      'reply': result.data
+    })
+  } else if (result.statusCode == 400) {
+    res.render('findloan', {
+      'message': 'Invalid SSN'
+    })
+  }
+
 });
 
 
